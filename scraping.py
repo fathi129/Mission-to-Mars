bb8fe62596bfbfd4d7902bd87a57f9468c48c268
fathi129 <nasreen.fathi@gmail.com>
2022-05-28 15:54:05 -0500

    Updated the files

--- scraping.py
+++ scraping.py
@@ -1,144 +1,62 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[57]:
-
 
 from splinter import Browser
 from bs4 import BeautifulSoup as soup 
 from webdriver_manager.chrome import ChromeDriverManager
+import datetime as dt
 import pandas as pd
-
-
-# In[2]:
-
-
-executable_path = {'executable_path':ChromeDriverManager().install()}
-browser = Browser('chrome',**executable_path,headless=False)
-
-
-# In[13]:
-
-
-url = 'https://redplanetscience.com' 
-browser.visit(url)
-browser.is_element_present_by_css('div.list_text',wait_time=1)
-
-
-# In[14]:
-
-
-html = browser.html
-
-
-# In[15]:
-
-
-html[0:500]
-
-
-# In[16]:
-
-
-news_soup = soup(html,'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
-
-
-# In[24]:
-
-
-
-# content = slide_elem.find('div',class_ = 'content_title')
-# text = content.text
-# print(text)
-
-news_title = slide_elem.find('div',class_ = 'content_title').get_text()
-news_title
-
-
-# In[28]:
-
-
-news_p = slide_elem.find('div',class_='article_teaser_body').get_text()
-news_p
-
-
+def scrape_all():
+    executable_path = {'executable_path':ChromeDriverManager().install()}
+    browser = Browser('chrome',**executable_path,headless=False)
+    news_title, news_paragraph = mars_news(browser)
+    data = {
+        "news_title":news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    browser.quit()
+    return data
+def mars_news(browser):
+    url = 'https://redplanetscience.com' 
+    browser.visit(url)
+    browser.is_element_present_by_css('div.list_text',wait_time=1)
+    html = browser.html
+    news_soup = soup(html,'html.parser')
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        news_title = slide_elem.find('div',class_ = 'content_title').get_text()
+        news_p = slide_elem.find('div',class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None,None   
+    return news_title,news_p
 # ### Featured Images
-
-# In[50]:
-
-
-url = 'https://spaceimages-mars.com/'
-browser.visit(url)
-
-# html[0:500]
-
-
-# In[51]:
-
-
-
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
-
-
-# In[52]:
-
-
-html = browser.html
-img_soup = soup(html,'html.parser')
-
-
-# In[55]:
-
-
-img_url_rel = img_soup.find('img',class_='fancybox-image').get('src')
-img_url_rel
-# url + img_url_rel
-
-
-# In[56]:
-
-
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
-
-
-# In[66]:
-
-
-df = pd.read_html('https://galaxyfacts-mars.com/')[0]
-df
-
-
-# In[67]:
-
-
-df.columns = ['Description','Mars','Earth']
-df
-
-
-# In[68]:
-
-
-df.set_index('Description',inplace = True)
-df
-
-
-# In[69]:
-
-
-df.to_html()
-
-
-# In[70]:
-
-
-browser.quit()
-
-
-# In[ ]:
+def featured_image(browser):
+    url = 'https://spaceimages-mars.com/'
+    browser.visit(url)
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+    html = browser.html
+    img_soup = soup(html,'html.parser')
+    try:
+        img_url_rel = img_soup.find('img',class_='fancybox-image').get('src')
+        img_url_rel
+        # url + img_url_rel
+    except AttributeError:
+        return None  
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}' 
+    return img_url
+def mars_facts():
+    try:    
+        df = pd.read_html('https://galaxyfacts-mars.com/')[0]
+    except BaseException:
+        return None    
+    df.columns = ['Description','Mars','Earth']
+    df.set_index('Description',inplace = True)
+    return df.to_html()
+#if __name__ == "__main__":
+#     print(scrape_all())    
 
 
 
-
+   
